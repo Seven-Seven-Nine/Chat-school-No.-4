d6d7bf907db9951846ad752f23b3ff6843fc4ae3
Seven-Seven-Nine <danila.umbetaliev.05@mail.ru>
2025-04-04 16:29:43 +0500

    A copy of the new database has been added.

--- config/chat-school-4.sql
+++ config/chat-school-4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Мар 30 2025 г., 15:37
+-- Время создания: Апр 04 2025 г., 13:28
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -40,9 +40,11 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id_news`, `id_user`, `title`, `date`, `text`) VALUES
-(7, 1, 'Тестовая новость 1', '2025-03-30', 'Текст тестовой новости 1.\r\n\r\nВторой абзац тестовой новости 1.'),
-(8, 1, 'Тестовая новость 2', '2025-03-30', 'Текст тестовой новости 2.\r\n\r\nВторой абзац тестовой новости 2.'),
-(9, 1, 'Тестовая новость 3', '2025-03-30', 'Текст тестовой новости 3.\r\n\r\nВторой абзац тестовой новости 3.');
+(7, 1, 'Тестовая новость 1', '2025-03-30', '<p>Текст тестовой новости 1.</p>\r\n\r\n<p>Второй абзац тестовой новости 1.</p>'),
+(8, 1, 'Тестовая новость 2', '2025-03-30', '<p>Текст тестовой новости 2.</p>\r\n\r\n<p>Второй абзац тестовой новости 2.</p>'),
+(9, 1, 'Тестовая новость 3', '2025-03-30', '<p>Текст тестовой новости 3.</p>\r\n\r\n<p>Второй абзац тестовой новости 3.</p>'),
+(14, 1, 'Большая текстовая новость', '2025-04-04', '<p>Большая текстовая новость.</p>\r\n\r\n<p>Я серьёзно, это очень большая текстовая новость.</p>\r\n\r\n<p>Даже не пытайся её читать, ты умрёшь от старости! Или от спида. Или от брюшного тифа.</p>\r\n\r\n<p>Хватит читать! Читать вредно для тупых, вдруг они поумнеют!</p>\r\n<p>Остановись...</p>\r\n\r\n<p><b>You don\'t puss me!</p>\r\n\r\n<p>Press F to respect! Press F to respect! Press F to respect!</b></p>'),
+(15, 1, 'Теги', '2025-04-04', '<p>Текст</p>\r\n<h3>Заголовок третьего уровня</h3>\r\n<p><b>Жирный текст!</b> Прям как ты.</p>');
 
 -- --------------------------------------------------------
 
@@ -63,8 +65,7 @@ CREATE TABLE `updates` (
 --
 
 INSERT INTO `updates` (`id_update`, `id_user`, `title`, `date`, `text`) VALUES
-(4, 1, 'Updates | Hot-fix', '2025-03-30', '1) Добавлено это.\r\n2) Удалено это.\r\n3) Изменено вот это.'),
-(5, 1, 'Updates | Hot-fix', '2025-03-31', 'АААА');
+(4, 1, 'Updates | Hot-fix', '2025-03-30', '<ul>\r\n<li>Изменено вот это.</li>\r\n<li>Добавлено вот это.</li>\r\n<li>Удалено вот это.</li>\r\n</ul>');
 
 -- --------------------------------------------------------
 
@@ -78,6 +79,7 @@ CREATE TABLE `users` (
   `role` varchar(20) NOT NULL DEFAULT 'user' COMMENT 'Роль пользователя',
   `email` varchar(50) NOT NULL COMMENT 'Почта пользователя',
   `password` varchar(120) NOT NULL COMMENT 'Пароль пользователя',
+  `path_to_image` varchar(120) NOT NULL DEFAULT 'null' COMMENT 'Изображение пользователя',
   `token` varchar(120) NOT NULL COMMENT 'Токен пользователя'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -85,10 +87,9 @@ CREATE TABLE `users` (
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id_user`, `login`, `role`, `email`, `password`, `token`) VALUES
-(1, 'admin', 'administrator', 'admin@mail.ru', '$2y$10$IwZbfwIfsRVTRwWrI528eOngo7QisbCGpRyfnFS2NG2A8alcPid0G', '308b1bdd2d252ae803e7b3dfa6f1badb'),
-(2, 'test3', 'user', 'test3@mail.ru', '$2y$10$7DZooDFT9YyVdN702BZYueuw0CxtZ0GEJ1s7cSeX4AJrnsjiJuDsq', '9fa62ca014dd1223f6971b9724adc986'),
-(3, 'test', 'user', 'test@mail.ru', '$2y$10$cmp75TItO4zxe.o0GlW4Pu926yld/8atArH6bcUoNQYS33b.y41Ue', '51e568d9593e44db324f0cdf3a9ab4b4');
+INSERT INTO `users` (`id_user`, `login`, `role`, `email`, `password`, `path_to_image`, `token`) VALUES
+(1, 'admin', 'administrator', 'admin@mail.ru', '$2y$10$IwZbfwIfsRVTRwWrI528eOngo7QisbCGpRyfnFS2NG2A8alcPid0G', '/user_images/devushka_krolik_schaste_206996_1920x1200.jpg', '6fd79423b876a74d6ae2de3d482a48eb'),
+(3, 'test', 'user', 'test@mail.ru', '$2y$10$cmp75TItO4zxe.o0GlW4Pu926yld/8atArH6bcUoNQYS33b.y41Ue', '/user_images/malchik_ulybka_sobaka_1006791_1920x1080.jpg', '405d0cf6f2ddd0a36fa8bfa69bae95a2');
 
 --
 -- Индексы сохранённых таблиц
@@ -122,19 +123,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `news`
 --
 ALTER TABLE `news`
-  MODIFY `id_news` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_news` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT для таблицы `updates`
 --
 ALTER TABLE `updates`
-  MODIFY `id_update` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_update` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
